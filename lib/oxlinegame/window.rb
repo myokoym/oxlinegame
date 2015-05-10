443a4b5fa0ec3adb2029cbe3146dd85dfd993318
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-05-10 12:37:22 +0900

    Set options to default values

--- lib/oxlinegame/window.rb
+++ lib/oxlinegame/window.rb
@@ -5,6 +5,9 @@ module Oxlinegame
   class Window < Onsengame::Window
     def initialize(options={})
       super
+      @options[:n_rows] ||= 3
+      @options[:n_win_cells] ||= 3
+      @options[:n_players] ||= 2
       self.caption = "Oxline"
       @scenes = []
       @scenes << Scene::Title.new(self)
